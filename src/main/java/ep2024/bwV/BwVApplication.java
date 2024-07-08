81aef4741a323843b7954cc6501a7ad749527404
Emanuele-P <161461547+Emanuele-P@users.noreply.github.com>
2024-07-08 10:21:37 +0200

    Initial commit

---
+++ src/main/java/ep2024/bwV/BwVApplication.java
@@ -0,0 +1,13 @@
+package ep2024.bwV;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BwVApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BwVApplication.class, args);
+	}
+
+}
